a66cfe24b4b02d338fc654eb2b31f9d2eefc4a6c
javier guerra <javierguerra2203@gmail.com>
2021-02-24 21:09:06 -0500

    final result

--- src/points/Point9.java
+++ src/points/Point9.java
@@ -16,8 +16,29 @@ public class Point9 {
     public static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("\n\t\tPUNTO 9");
+        System.out.print("Numero de dias del viaje: ");
+        int travelDays = input.nextInt();
 
+        System.out.print("Cuantos kilometros conducio al dia?: ");
+        float kmTraveled = input.nextFloat();
+
+        System.out.print("Cuantos cuesta el litro de gasolina?: ");
+        float gasPrice = input.nextFloat();
+
+        System.out.print("Ingrese el promedio de kilometro por litro de gasolina: ");
+        float kmPerLiter = input.nextFloat();
+
+        System.out.print("Cuanto pago por estacionamiento diario?: ");
+        float parkingPrice = input.nextFloat();
+
+        System.out.print("Cuanto pago por peajes?: ");
+        float tollPrice = input.nextFloat();
+
+        float totalGas = (kmTraveled/kmPerLiter) * gasPrice;
+        float pathCharge = parkingPrice + tollPrice;
+        float totalTrip = (totalGas + pathCharge) * travelDays;
+
+        System.out.printf("El total gastado en el viaje es de: %f\n", totalTrip);
     }
-
-
 }
